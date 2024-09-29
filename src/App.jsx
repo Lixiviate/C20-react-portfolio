3f23b3759172983f57f79041012f23e07cbf21b9
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-09-29 16:22:21 -0600

    Add header above nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Nav from "./components/NavTabs";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Nav />
       <main className="mx-3">
         <Outlet />
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
+      <h1>Lixiviate</h1>
+    </header>
+  );
+}
+
+export default Header;
