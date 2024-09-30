8fb2cd3189d96de056503476d20d88a210c98e76
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-09-29 20:03:43 -0600

    Update resume tab with proficiencies and resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,22 +1,50 @@
+import gitResume from "../assets/gitResume.pdf";
+
 export default function Resume() {
   return (
     <div>
-      <h1>Resume Page</h1>
+      <h1>Resume</h1>
       <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
+        Download my{" "}
+        <a href={gitResume} download>
+          resume
+        </a>
       </p>
+      <h2>Proficiencies</h2>
+
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <h3>Front-end</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Back-end</h3>
+          <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>MySQL</li>
+            <li>MongoDB</li>
+            <li>RESTful APIs</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Other Skills</h3>
+          <ul>
+            <li>Git</li>
+            <li>Agile Development</li>
+            <li>Responsive Design</li>
+            <li>Test-Driven Development</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
